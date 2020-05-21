b988e749ddcdf06aa02f72a53d88f97f2cb8629b
GoncaloPerna <gonperna@gmail.com>
2020-05-21 22:01:59 +0100

    more changes to the layout from categories

--- lib/view/update_categories_view.dart
+++ lib/view/update_categories_view.dart
@@ -24,25 +24,41 @@ class _UpdateCategories extends State<StatefulWidget> {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: Text("Updating Categories"),
+        title: Text("Settings"),
       ),
       body:
         Column(
           children: <Widget>[
+            Container(
+                color: Colors.blue,
+                child:
+                    Row(
+                      children: <Widget>[
+                      Padding(
+                        padding: EdgeInsets.only(left: 15.0),
+                        child: Text("Categories", style: TextStyle(fontSize: 17, color: Colors.white),),
+                      ),
+                      Padding(
+                        padding: EdgeInsets.only(left: 220.0),
+                        child: RaisedButton(
+                          color: Colors.white,
+                          onPressed: () {
+                          },
+                          child: Text("Add", style: TextStyle(fontWeight: FontWeight.bold),),
+                          ),
+                      ),
+                      ],
+                    ),
+            ),
             Expanded(
               child:
               ListView(
                 scrollDirection: Axis.vertical,
                 shrinkWrap: true,
                 children: <Widget>[
-                  for (String s in Data.expenseCategories)
-                    ListTile(
-                      onTap: (){
-                        createCategoriesFormPopUp(context);
-                      },
-                      leading: Icon(Icons.map),
-                      title: Text(s),
-                    ),
+                  for (int i = 0; i<Data.expenseCategories.length; i++)
+                    MyTile(i, temp_categories)
+
                 ],
               ),
             ),
@@ -70,7 +86,54 @@ class _UpdateCategories extends State<StatefulWidget> {
   }
 
 
-  createCategoriesFormPopUp(BuildContext context){
+
+}
+
+
+class MyTile extends StatefulWidget{
+
+  int id;
+  List<String> temp_categories;
+
+  MyTile(id, temp_categories){
+    this.id = id;
+    this.temp_categories = temp_categories;
+  }
+
+  @override
+  State<StatefulWidget> createState() => _MyTile(id, temp_categories);
+}
+
+
+class _MyTile extends State<StatefulWidget> {
+
+  int id;
+  List<String> temp_categories;
+
+  _MyTile(id, temp_categories){
+    this.id = id;
+    this.temp_categories = temp_categories;
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return ListTile(
+        onTap: (){
+          createCategoriesFormPopUp(context).then((onValue){
+            setState((){
+              if (onValue != null ){
+                temp_categories[id] = onValue;
+              }
+            });
+          });
+        },
+        leading: Icon(Icons.map),
+        title: Text(temp_categories[id]),
+    );
+  }
+
+
+  Future<String> createCategoriesFormPopUp(BuildContext context){
 
     TextEditingController customController = TextEditingController();
 
@@ -85,10 +148,20 @@ class _UpdateCategories extends State<StatefulWidget> {
               elevation: 5.0,
               child: Text("Submit"),
               onPressed: () {
+                Navigator.of(context).pop(customController.text.toString());
+              },
+            ),
+            MaterialButton(
+              elevation: 5.0,
+              child: Text("Remove"),
+              onPressed: () {
               },
             )
           ]
       );
     });
-    }
+  }
+
 }
+
+
